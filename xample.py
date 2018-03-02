fbcba3d3a17e1953a55e8a9616e4031a9b04a186
杨竞颉 <j.yang19@ejm.org>
2018-03-02 19:49:01 +0800

    Improve interface: cache samples as json, support file matching

--- xample.py
+++ xample.py
@@ -1,37 +1,27 @@
 import re
 import sys
+import json
+import glob2
 import os.path
+import json.decoder
 import urllib.request
 from bs4 import BeautifulSoup
+from typing import Dict, List, Tuple
 from subprocess import Popen, PIPE, TimeoutExpired
-from typing import List, Tuple
 
 
 def run_file(py_file: str, ins: str = "") -> str:
     """Run python script with optional input to stdin and return output from stdout.
 
-    :param py_file: The name of the python script.
-                    Relative path will be tried first;
-                    if that fails absolute path will be used;
-                    if that fails too a FileNotFoundError is raised.
+    :param py_file: The path to the python script.
     :raise FileNotFoundError: When path 'py_file' cannot be resolved.
     :param ins: The input fed to stdin as a string.
                 Defaults to empty string (no input).
     :return: The output from stdout as a string.
     """
-    # Resolve file path
-    if os.path.isfile(py_file):
-        path = py_file  # as absolute path
-    else:
-        rel_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), py_file)
-        if os.path.isfile(rel_path):
-            path = rel_path
-        else:
-            raise FileNotFoundError
-
     # Create subprocess
     process = Popen(
-        ["python3", path],
+        ["python3", py_file],
         stdin=PIPE,
         stdout=PIPE
     )
@@ -44,42 +34,42 @@ def run_file(py_file: str, ins: str = "") -> str:
         return outs.decode()
 
 
-def get_samples() -> List[Tuple[str, str]]:
-    """Get the sample inputs/outputs from samples.txt.
+def process_samples(raw_text: str) -> List[Dict[str, str]]:
+    """Convert the raw sample text into a list of {'in': in, 'out', out}.
 
+    :param raw_text: The raw sample text to be processed.
     :return: The sample inputs/outputs as a list of tuples of the the form (in, out).
     """
     samples = []
-    with open("samples.txt") as f:
-        taking_input = False
-        taking_output = False
-        in_ = ""  # Note: it is assumed that there are no empty inputs
-        out = ""  # or empty outputs
-        for line in f.readlines():
-            if line == "Exemple d'entrée\n" or line == "Sample input\n":
-                taking_input = True
-                taking_output = False
-                if out:
-                    samples.append((in_, out))
-                    in_, out = "", ""
-                continue
-            if line == "Exemple de sortie\n" or line == "Sample output\n":
-                taking_input = False
-                taking_output = True
-                continue
-            if line == "Commentaire\n" or line == "Note\n":
-                taking_input = False
-                taking_output = False
-                samples.append((in_, out))
+    taking_input = False
+    taking_output = False
+    in_ = ""  # Note: it is assumed that there are no empty inputs
+    out = ""  # or empty outputs
+    for line in raw_text.split('\n'):
+        if line == "Exemple d'entrée" or line == "Sample input":
+            taking_input = True
+            taking_output = False
+            if out:
+                samples.append({"in": in_, "out": out})
                 in_, out = "", ""
+            continue
+        if line == "Exemple de sortie" or line == "Sample output":
+            taking_input = False
+            taking_output = True
+            continue
+        if line == "Commentaire" or line == "Note":
+            taking_input = False
+            taking_output = False
+            samples.append({'in': in_, 'out': out})
+            in_, out = "", ""
 
-            if line != '\n':
-                if taking_input:
-                    in_ += line
-                if taking_output:
-                    out += line
+        if line:
+            if taking_input:
+                in_ += line + "\n"
+            if taking_output:
+                out += line + "\n"
     if taking_output:
-        samples.append((in_, out))
+        samples.append({'in': in_, 'out': out})
     return samples
 
 
@@ -98,7 +88,9 @@ def check_sample(in_: str, expected_out: str, py_file: str) -> Tuple[bool, str]:
 
 def check_samples(py_file: str):
     """Run check_sample over all the examples and display nice error messages."""
-    for in_, expected_out in get_samples():
+    samples = get_samples(py_file)
+    for sample in samples:
+        in_, expected_out = sample['in'], sample['out']
         out = ""
         try:
             passed, out = check_sample(in_, expected_out, py_file)
@@ -116,8 +108,9 @@ def check_samples(py_file: str):
             print(e)
         else:
             print("✅ Passed")
-    if not get_samples():
+    if not samples:
         print("⚠️ No tests found.")
+    print()
 
 
 def fetch_samples(url: str) -> str:
@@ -130,37 +123,48 @@ def fetch_samples(url: str) -> str:
     return BeautifulSoup(urllib.request.urlopen(url).read(), "html.parser").find(id="samples").text
 
 
+def get_samples(py_file: str) -> List[Dict[str, str]]:
+    """Get samples from samples.json using the url in the script (calling load_samples before ensures that it exists).
+
+    :param py_file: The path to the python script.
+    :return: The samples in samples.json matched by the url on the first line of the script.
+    """
+    with open(py_file) as f:
+        url = re.search(r"https?://prologin.org/train/20\d{2}/(qualification|semifinal)/[a-z_A-Z\d]*", f.readline())[0]
+    with open("samples.json") as f2:
+        return json.load(f2)[url]
+
+
 def load_samples(py_file: str) -> bool:
-    """Read the samples from the url specified in first one of the solution file and write it to samples.txt.
+    """Read the samples from the url specified in first one of the solution file and write it to samples.json.
 
     :param py_file: The path to the solution python script.
     :return: True if the samples are successfully loaded from the specified url; False otherwise.
     """
-    print('--')
+    print("#" * 80)
+    print(py_file)
     with open(py_file) as f:
         code = f.readlines()
     if code and re.search(r"https?://prologin.org/train/20\d{2}/(qualification|semifinal)/[a-z_A-Z\d]*", code[0]):
         url = re.search(r"https?://prologin.org/train/20\d{2}/(qualification|semifinal)/[a-z_A-Z\d]*", code[0])[0]
         print(f"✅ Found url: {url}")
         try:
-            with open("samples.txt") as f:
-                local_samples = f.readlines()
-        except FileNotFoundError:
-            # file doesn't exist yet
-            local_samples = None
+            with open("samples.json") as f:
+                local_samples = json.load(f)
+                if type(local_samples) != dict:
+                    raise json.decoder.JSONDecodeError
+        except (FileNotFoundError, json.decoder.JSONDecodeError):  # file doesn't exist yet or invalid json
+            local_samples = {}
+            print("⚠️ Cannot read samples.json; the file is reinitialized.")
 
-        if local_samples:
-            url_loaded = local_samples[0].strip('\n')
-            if url_loaded == url:
-                print(f"⚠️ To reload examples, remove the url on the first line in samples.txt.")
-                print("--\n")
-                return True
-
-        samples = fetch_samples(url)
-        with open("samples.txt", "w") as f:
-            f.write(url + '\n')
-            f.write(samples)
-        print(f"✅ Wrote samples to samples.txt")
+        if url in local_samples:
+            print(f"⚠️ Reading examples from samples.json; delete entry in samples.json to reload.")
+            print("--\n")
+            return True
+        local_samples[url] = process_samples(fetch_samples(url))
+        with open("samples.json", "w") as f:
+            json.dump(local_samples, f, indent=2)
+        print(f"✅ Wrote samples to samples.json")
         print(f"✅ Saved url: {url}")
     else:
         print("❌️ Failed to locate url on first line of script.")
@@ -176,12 +180,11 @@ def test(py_file: str):
 
 
 if __name__ == '__main__':
-    try:
-        sys.argv[1]
-    except IndexError:
-        print("❌️ Path to solution code must be passed as a command line argument.")
-    else:
-        if os.path.isfile(sys.argv[1]):
-            test(sys.argv[1])
-        else:
-            print(f"❌ Cannot find script {sys.argv[1]}.")
+    path = sys.argv[1] if len(sys.argv) > 1 else '**/'
+    path += "/" if os.path.isdir(path) and not path.endswith("/") else ""
+    path += "*.py" if path.endswith("/") else ""
+    files: List[str] = sorted(glob2.glob(path))
+    if not files:
+        print(f"⚠️ {path} does not match any file.")
+    for file in files:
+        test(file)
